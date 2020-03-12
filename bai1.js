d327d4f213bf998a2dfa601ee928cad089b70496
nguyenmanhhung16021997 <nguyenmanhhung16021997@gmail.com>
2020-03-12 12:24:40 +0700

    bai tap cau truc dieu kien 1

---
+++ bai1.js
@@ -0,0 +1,9 @@
+
+let a = Number(prompt("nhap a"));
+let b = Number(prompt("nhap b"));
+let c = a % b;
+if ( c == 0 ){
+    document.write("a chia hết cho b");
+} else {
+    document.write("a không chia hết cho b");
+}
